de28db791316031259c5a548fe32f0beb697e565
Albin Ljunghusen <albin.ljunghusen@isotop.se>
2022-02-06 12:22:01 +0100

    Add Documents/PowerShell/Microsoft.PowerShell_profile.ps1

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+Set-Alias rc Edit-PowershellProfile
+
+
+function Prompt
+{
+    $mywd = (Get-Location).Path
+    $mywd = $mywd.Replace( $HOME, '~' )
+    Write-Host "PS " -NoNewline -ForegroundColor DarkGreen
+    Write-Host ("" + $mywd + ">") -NoNewline -ForegroundColor Green
+    return " "
+}
+
+function Edit-PowershellProfile
+{
+    nvim $Profile
+}
+
+oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/hunk.omp.json | Invoke-Expression
