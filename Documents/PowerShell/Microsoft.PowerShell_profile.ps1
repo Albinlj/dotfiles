e50360d8eeef74adf3f4bc6f8d1701b1d9ab4d9e
Albin Ljunghusen <albin.ljunghusen@isotop.se>
2022-02-06 12:52:11 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -23,6 +23,7 @@ Set-Alias vimconf Edit-NeovimConfig
 function Edit-NeovimConfig
 {
     chezmoi edit ~/.local/share/chezmoi/.chezmoitemplates/init.vim
+    chezmoi apply
 }
 
 oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/hunk.omp.json | Invoke-Expression
